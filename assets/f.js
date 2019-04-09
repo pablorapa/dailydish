973dec1397bd329e58d52f8e939bde43be18e1aa
pablorapa <pablo.rapalin@gmail.com>
2019-04-09 13:47:12 -0300

    Add files via upload

--- assets/f.js
+++ assets/f.js
@@ -2,25 +2,13 @@ $( document ).ready(function() {
    
 	$("#cerrar").click(function(e) {
 		e.preventDefault();
-        $("aside form").hide();
+        $(".header nav ul").hide();
         $("html").css('overflow','auto');
-    });
-    
-    $("#masInfo").click(function(e) {
-		e.preventDefault();
-        $("aside form").hide();
-        $("html").css('overflow','auto');
-    });
-    
-    $("#contame").click(function(e) {
-		e.preventDefault();
-        $("aside form").show();
-        $("html").css('overflow','hidden');
     });
     
     $("#hamb").click(function(e) {
 		e.preventDefault();
-        $("header nav ul").toggle();
+        $(".header nav ul").show();
 	});
 
 });
